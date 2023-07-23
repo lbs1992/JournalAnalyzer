e6154ce0dd15ba499827e1e73b3355cd4c4933e3
Luke Simon <lukesimon@Lukes-MBP.localdomain>
2023-07-22 19:56:06 -0500

    Add keyword searching

--- api.py
+++ api.py
@@ -8,6 +8,20 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import calendar
 
+class JournalEntry: 
+    def __init__(self, month, day, year, entry):
+        self._entry = entry #fix_spelling_and_grammar(entry)
+        self._date = datetime.datetime(year, month, day)
+
+    def entry(self): 
+        return self._entry
+
+    def pretty_date(self): 
+        return self._date.strftime("%B %d, %Y")
+
+    def date(self):
+        return self._date
+
 def init_database():    
     if os.path.exists("journal.db"):
         return
@@ -29,50 +43,25 @@ def init_database():
 
     conn.close()
 
-API_KEY = open('key.txt','r').readline()
-
-class JournalEntry: 
-    def __init__(self, month, day, year, entry):
-        self._entry = entry #fix_spelling_and_grammar(entry)
-        self._date = datetime.datetime(year, month, day)
-
-    def entry(self): 
-        return self._entry
-
-    def pretty_date(self): 
-        return self._date.strftime("%B %d, %Y")
-
-    def date(self):
-        return self._date
-
-def get_database_range():  
-    # Connect to the database
-    connection = sqlite3.connect('journal.db')
-    cursor = connection.cursor()
-
-    # Query the earliest and latest dates
-    cursor.execute("SELECT MIN(month, day, year), MAX(month, day, year) FROM journal")
-    result = cursor.fetchone()
-
-    earliest_date = tuple(result[0:3])
-    latest_date = tuple(result[3:6])
-
-    # Close the database connection
-    cursor.close()
-    connection.close()
-
-    return
+API_KEY = open('key.txt','r').readline().strip()
 
 def analyze_journal_entries(entries, question):
-    prompt = "Use the following journal entries for answers to these questions: "
-    prompt = ""
+    prompt = """
+        Task: Analyze the provided text entries and extract relevant insights, based on the given question. 
 
+        Context: The provided text entries are daily journal entries covering various topics, experiences, emotions, etc. 
+        You can assume I wrote them.
+
+        The requirements for your response are:
+            - Use no other context or knowledge except for what is found in the provided text
+    """
+
+    text_entries = ""
     for entry in entries:
-        prompt += entry.pretty_date() + "\n"
-        prompt += entry.entry() + "\n"*2
-
-    prompt += "\n"*2
-    prompt += "Prompt: " + question
+        text_entries += entry.pretty_date() + "\n"
+        text_entries += entry.entry() + "\n"*2
+    prompt += f"Text entries: \nSTART\n\n{text_entries}\n\nEND"
+    prompt += f"\nQuestion: {question}\n" 
     
     openai.api_key = API_KEY
     completion = openai.ChatCompletion.create(
@@ -80,31 +69,37 @@ def analyze_journal_entries(entries, question):
       temperature = 0.8,
       max_tokens = 2000,
       messages = [
-        {'role':'system', 'content':'You are an AI assistant who knows everything about me. Use 2nd person in your responses.'},
         {"role": "user", "content": prompt}
       ]
     )
 
     return completion.choices[0].message.content
 
+def remove_non_letters(input_string):
+    # Use regex pattern to match all non-letter characters (anything that's not a-z or A-Z)
+    pattern = r'[^a-zA-Z 0-9]'
+    
+    # Replace all non-letter characters with an empty string
+    result = re.sub(pattern, '', input_string)
+    return result
 
-def fix_spelling_and_grammar(entry):
-    print ("Fixing the spelling and grammar", end="...")
-    resp = get_chatgpt_response("Fix the grammar and spelling: " + entry)
-    print ("DONE")
-    return resp
+def clean_up_text(entry, fix_spelling = True, fix_punctuation = True, format_nicely = True, fix_grammar = False):
+    prompt = "Fix the following: "
+    things_to_fix = []
 
-def fix_spelling(entry):
-    print ("Fixing the spelling", end="...")
-    resp = get_chatgpt_response("Fix the spelling: " + entry)
-    print ("DONE")
-    return resp
+    if fix_spelling: 
+        things_to_fix.append("spelling")
+    if fix_punctuation:
+        things_to_fix.append("punctuation")
+    if fix_grammar:
+        things_to_fix.append("grammar")
+    
+    prompt += ",".join(things_to_fix)
+    
+    if format_nicely:
+        prompt += ". Format the text nicely."
 
-def fix_spelling_and_punctuation(entry):
-    print ("Fixing the spelling and punctuation", end="...")
-    resp = get_chatgpt_response("Fix the spelling and punctuation: " + entry)
-    print ("DONE")
-    return resp
+    return get_chatgpt_response(prompt + "\n\nText:\"\"\"\n" + entry + "\"\"\"")
 
 def get_journal_entries_by_date_range(start_month, start_day, start_year, end_month, end_day, end_year):
     conn = sqlite3.connect('journal.db')
@@ -122,7 +117,7 @@ def get_journal_entries_by_date_range(start_month, start_day, start_year, end_mo
     journal_entries = []
 
     for entry in entries:
-        journal_entries.append(JournalEntry(*entry))
+        journal_entries.append(JournalEntry(*entry[0:4]))
     
     return journal_entries
 
@@ -157,18 +152,65 @@ def split_audio_file(original_file, silent_threshold = 1500, silence_def = 16):
     
     return segment_paths
 
+def get_database_range():  
+    # Connect to the database
+    conn = sqlite3.connect('journal.db')  # Replace 'your_database_name.db' with the actual database file name
+    cursor = conn.cursor()
+
+    # Get the minimum entry
+    cursor.execute('SELECT * FROM journal ORDER BY year, month, day ASC LIMIT 1')
+    min_entry = cursor.fetchone()
+
+    # Get the maximum entry
+    cursor.execute('SELECT * FROM journal ORDER BY year DESC, month DESC, day DESC LIMIT 1')
+    max_entry = cursor.fetchone()
+
+    # Close the connection
+    conn.close()
+
+    min_month, min_day, min_year, _, _ = min_entry
+    max_month, max_day, max_year, _, _ = max_entry
+
+    return (datetime.date(min_year, min_month, min_day), datetime.date(max_year, max_month, max_day))
+
+def find_keyword_search(query):
+    prompt = """
+        Task: Return the word I need to use in a search. If there is no word to search, return "None"
+
+        Context: I am going to give you a question or a statement that requires searching something. I want you to analyze the statement and return me the word or phrase I need to use in the search.
+
+        Format: <the word or phrase>
+
+        Requirements: 
+         - Only return the word or phrase, no other context
+
+        Prompt: 
+    """
+
+    prompt += query
+
+    return get_chatgpt_response(prompt)
+
 def get_date_range(query):
     prefix = """
-        Give me a date range that the text applies to. 
-        An example output from you would be (1,4,1992,1,3,2022). 
-        Use month-day format. Do not pad any numbers with zeros. 
-        Your response must contain between 19 and 23 characters, with no explanation.
+        Task: Return the date range for the given question or statement.
 
-        Generic format: \"\"\"
-        (From month, from day, from year, to month, to day, to year)
-        \"\"\"
+        Context: I am going to give you a question or a statement that requires knowledge of a certain date. I want
+        you to analyze the statement and return me a date range. 
 
-        Text: \"\"\"
+        Format: (<from month>,<from day>,<from year>,<to month>,<to day>,<to year>). 
+
+        Requirements: 
+          - Numbers must not start with 0; do not 0-pad
+          - Use month-day format
+          - Always include the day 
+          - No whitespace
+          - Minimum character count is 19, maximum is 23
+          - No explanation
+          - Six numbers separated by a comma and in parenthesis
+          - If no response, return "None"
+
+        Prompt: \"\"\"
     """
     
 
@@ -214,7 +256,6 @@ def remove_suffixes(input_string):
     return re.sub(pattern, r"\1", input_string)
 
 def has_date(string):
-    print ("Parsing the journal entry for a date", end="...")
     contains_date = False
     days = calendar.day_name
     months = calendar.month_name[1:]
@@ -222,17 +263,16 @@ def has_date(string):
     regex_days = r"|".join(days).lower()
     regex_months = r"|".join(months).lower()
     
-    text = "(" + regex_days + "){0,1}[,\\s]*" + "(?P<month>" + regex_months + ")\\s*(?P<day>\\d{1,2}(st|nd|rd|th){0,1})\\s*,?\\s*(?P<year>\\d{4})\\.*"
+    text = "^(" + regex_days + "){0,1}[,\\s]*" + "(?P<month>" + regex_months + ")\\s*(?P<day>\\d{1,2}(st|nd|rd|th){0,1})\\s*,?\\s*(?P<year>\\d{4})\\.*"
     
-    match = re.search(text, string)
+    match = re.search(text, string.strip())
     if match:
+        print (f"Found date: {string}")
         month = match.groupdict()['month']
         month = month[0].upper() + month[1:]
         day = int(remove_suffixes(match.groupdict()['day']))
         year = int(match.groupdict()['year'])
-        print(f"{month} {day}, {year}")
         return (True, months.index(month) + 1, day, year, string.replace(match.group(), "").strip())
-    print ("No date found")
     return (False, -1, -1, -1, None)
 
 def transcribe(file): 
@@ -257,10 +297,12 @@ def get_chatgpt_response(chat_input):
 def parse_and_add_journal_to_database(journal_text):
     init_database()
     
-    new_period = "PERIOD_PERIOD_PERIOD_ALERT"
-    journal_text = journal_text.replace("\n", " ")
-    journal_text = re.sub(r"([^\.]*)\.([^\.])", r"\1" + new_period + r"\2", journal_text)
-    lines = journal_text.split(new_period)
+    # new_period = "PERIOD_PERIOD_PERIOD_ALERT"
+    # journal_text = journal_text.replace("\n", " ")
+    # journal_text = re.sub(r"([^\.]*)\.([^\.])", r"\1" + new_period + r"\2", journal_text)
+    lines = journal_text.split("\n")
+    lines = list(filter(lambda line: line, lines))
+
 
     transcribing_entry = False
     date = (-1, -1, -1)
@@ -273,7 +315,7 @@ def parse_and_add_journal_to_database(journal_text):
         date = (month, day, year)
         entry_lines += entry + ". "
       elif contains_date and transcribing_entry: #Let's populate this new entry
-        add_journal_entry_to_database(JournalEntry(*date, fix_spelling_and_punctuation(entry_lines)))
+        add_journal_entry_to_database(JournalEntry(*date, clean_up_text(entry_lines)))
         entry_lines = ""
         date = (month, day, year)
         entry_lines += entry + ". "
--- main.py
+++ main.py
@@ -2,30 +2,33 @@ from api import *
 from collections import defaultdict
 import shutil
 import os
+import datetime
+from fuzzywuzzy import fuzz
 
-segment_paths = split_audio_file("/Users/lukesimon/Downloads/test.m4a")
+# segment_paths = split_audio_file("/Users/lukesimon/Downloads/test.m4a")
 
-all_entries = ""
+# all_entries = ""
 
 #TODO: What happens if there's only one entry?
-for path in segment_paths: 
-	transcription = transcribe(path)
-	_, month, day, year, content = has_date(transcription.lower())
-	new_name = f"{str(month).zfill(2)}_{str(day).zfill(2)}_{year}.mp3"
-	shutil.copy(path, os.path.join("/Users/lukesimon/Dev/JournalAnalyzer/audio_files", new_name))
+# for path in segment_paths: 
+# 	transcription = transcribe(path)
+# 	_, month, day, year, content = has_date(transcription.lower())
+# 	new_name = f"{str(month).zfill(2)}_{str(day).zfill(2)}_{year}.mp3"
+# 	shutil.copy(path, os.path.join("/Users/lukesimon/Dev/JournalAnalyzer/audio_files", new_name))
 
-	month_name = calendar.month_name[1:][month-1]
-	all_entries += f"{month_name} {day}, {year}\n{fix_spelling_and_punctuation(content)}\n\n"
+# 	month_name = calendar.month_name[1:][month-1]
+# 	all_entries += f"{month_name} {day}, {year}\n{fix_spelling_and_punctuation(content)}\n\n"
 
-print (all_entries)
+# print (all_entries)
 
-parse_and_add_journal_to_database(all_entries)
 
-# print(transcribe("input1.mp3"))
 # journal = ""
 # with open('SampleJournal.txt', 'r') as file:
 #     # Perform operations on the file
 #     journal = file.read()
+
+# parse_and_add_journal_to_database(journal)
+
 
 journal_prompt = input("Prompt: ")
 
@@ -38,6 +41,30 @@ if date_range:
     print (analyze_journal_entries(entries, journal_prompt))
   else: 
     print ("You have no journal entries associated with that prompt.")
+else: 
+	to_search = find_keyword_search(journal_prompt)
+	segment_length = len(to_search.split())
 
+	if to_search != "None":
+		print (to_search)
 
+	start, end = get_database_range()
+
+	entries = get_journal_entries_by_date_range(
+			start.month, start.day, start.year,
+			end.month, end.day, end.year
+	)
+
+	potential_entries = []
+	for entry in entries: 
+		segments = entry.entry().split()
+		for x in range(0, len(segments) - segment_length):
+			curr_segment = remove_non_letters(" ".join(segments[x:x+segment_length]))
+			ratio = fuzz.ratio(to_search.lower(), curr_segment.lower())
+			if ratio > 90: 
+				potential_entries.append(entry)
+				break
+	
+	import pdb; pdb.set_trace()
+	print(analyze_journal_entries(potential_entries, journal_prompt))		
 
